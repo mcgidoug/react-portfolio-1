e89557a2e7be93f99f58bbc76d15c6c9e4e83bef
mcgidoug <mcgidoug@gmail.com>
2022-07-19 12:02:24 -0500

    updated page for deployment

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>Portfolio Item 1</h3>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
