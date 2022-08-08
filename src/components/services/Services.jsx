a069438d55a8e3886fc031b89d51d8096060d920
mcgidoug <mcgidoug@gmail.com>
2022-08-08 16:58:41 -0500

    updated services info section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,19 +16,30 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Conducting usability tests to develop user-friendly interfaces.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Designing comprehensive wireframes to showcase website
+                functionality.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Researching and implementing new technologies to engage new
+                users.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Rapid prototyping to deliver a viable product within short
+                timeframes.
+              </p>
             </li>
           </ul>
         </article>
@@ -40,19 +51,31 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Developing and maintaining sites using HTML, CSS, JavaScript,
+                React and Git.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Writing clean, maintainable and testable code to ensure
+                collaboration among developers on the team.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Refactoring code to up to date syntax to provide future
+                developers with efficient code.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Collaborating with team members to design user interfaces that
+                are visually interesting and easy to use.
+              </p>
             </li>
           </ul>
         </article>
@@ -64,19 +87,28 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Building and maintaining databases using MySQL and MongoDB.</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Utilizing Express and NodeJS to develop servers to serve web
+                applications.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Developing platforms for e-commerce, social media, and book
+                libary systems.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>
+                Data aggregation and routing for full stack applications using
+                the MERN stack.
+              </p>
             </li>
           </ul>
         </article>
