9fd8d07d18c2627e83b5abf233d4e9068b56b515
mcgidoug <mcgidoug@gmail.com>
2023-05-19 16:24:22 -0500

    updated portfolio section with professional work

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,38 +1,54 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/drum-machine.jpg";
-import IMG2 from "../../assets/color-sheets.jpg";
-import IMG3 from "../../assets/phone-infront-computer-work.jpg";
-import IMG4 from "../../assets/laptop-typing-wide.jpg";
+import IMG1 from "../../assets/easy-light.png";
+import IMG2 from "../../assets/autentik.png";
+import IMG3 from "../../assets/lupita-hvac.png";
+import IMG4 from "../../assets/arzet-home.png";
+import IMG5 from "../../assets/claudineco.png";
+import IMG6 from "../../assets/bomba-portfolio.png";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Drum Machine",
-    github: "https://github.com/mcgidoug/beatmaker",
-    demo: "https://mcgidoug.github.io/drum-machine/",
+    title: "Easy Light",
+    // github: "https://github.com/mcgidoug/beatmaker",
+    demo: "https://easy-light.ch/",
   },
   {
     id: 2,
     image: IMG2,
-    title: "Palette Creator",
-    github: "https://github.com/mcgidoug/color-palette-chooser",
-    demo: "https://mcgidoug.github.io/palette-creator/",
+    title: "Autentik",
+    // github: "https://github.com/mcgidoug/color-palette-chooser",
+    demo: "https://autentik.ch/",
   },
   {
     id: 3,
     image: IMG3,
-    title: "Book Store",
-    github: "https://github.com/mcgidoug/mern-book-store",
-    demo: "https://youtu.be/c6B4uieq0Y8",
+    title: "Lupita's HVAC",
+    // github: "https://github.com/mcgidoug/mern-book-store",
+    demo: "https://lupitas-hvac.netlify.app/",
   },
   {
     id: 4,
     image: IMG4,
-    title: "Goal Saver",
-    github: "https://github.com/mcgidoug/mern-goal-saver",
-    demo: "https://youtu.be/Ye_qcW9fFsw",
+    title: "Arzet Home",
+    // github: "https://github.com/mcgidoug/mern-goal-saver",
+    demo: "https://arzethomeimprovement.com/index.html",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Claudine CO",
+    // github: "https://github.com/mcgidoug/mern-goal-saver",
+    demo: "https://fascinating-jalebi-848979.netlify.app/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Bomba Portfolio",
+    // github: "https://github.com/mcgidoug/mern-goal-saver",
+    demo: "https://josh-bomba.vercel.app/",
   },
 ];
 
@@ -43,7 +59,7 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, demo }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
@@ -51,21 +67,21 @@ const Portfolio = () => {
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-                <a
+                {/* <a
                   href={github}
                   className="btn"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   Github
-                </a>
+                </a> */}
                 <a
                   href={demo}
                   className="btn btn-primary"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Live Demo
+                  Visit Site
                 </a>
               </div>
             </article>
